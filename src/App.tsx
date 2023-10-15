7dd819ad5a9fd4e94ee5d79ce11f27db22a44d65
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-10-16 00:13:55 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// views
+import { About, Contact, Hero, Projects } from "./views";
+
+// components
+import { Menu } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Menu />
+    </>
   );
 }
 
---
+++ src/views/About.tsx
@@ -0,0 +1,82 @@
+// assets
+import aboutPageImg from "../assets/about-me-page.svg";
+import aboutIllustration from "../assets/about-illustration.svg";
+import facebookIcon from "../assets/facebook-icon.svg";
+import instagramIcon from "../assets/instagram-icon.svg";
+import twitterIcon from "../assets/twitter-icon.svg";
+import youtubeIcon from "../assets/youtube-icon.svg";
+
+import { SocialMediaIcon, Reveal } from "../components";
+import { motion } from "framer-motion";
+import { fadeIn, scale } from "../utils/variants";
+import { transition } from "../utils/transition";
+
+const About = () => {
+  return (
+    <div
+      id="about"
+      className="min-h-screen flex items-center justify-center relative"
+      style={{
+        background: `url(${aboutPageImg})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div
+        className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between
+          items-center xl:items-start gap-12 w-full py-16 px-12"
+      >
+        <div className="flex-1 flex flex-col gap-4">
+          <Reveal>
+            <h2
+              className="text-center xl:text-start text-4xl sm:text-5xl lg:text-[64px] 
+              font-bold text-textPrimary"
+            >
+              About <span className="text-secondary"> Yusasive</span>
+            </h2>
+          </Reveal>
+
+          <Reveal>
+            <p className="text-center xl:text-start text-base sm:text-lg text-textSecondary">
+            I am a committed FrontEnd Developer with 2years of experience in the tech industry. 
+            I am passionate and responsible for the design and implementation of user interfaces 
+            (UIs) and UI components using React, a front-end JavaScript library.
+            </p>
+          </Reveal>
+
+          <motion.div
+            variants={fadeIn("up")}
+            transition={transition()}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false }}
+            className="flex items-center justify-center xl:justify-start gap-6"
+          >
+          <SocialMediaIcon imgSrc={facebookIcon} title="Facebook" />
+          <SocialMediaIcon imgSrc={instagramIcon} title="Instagram" /> 
+          <SocialMediaIcon imgSrc={twitterIcon} title="Twitter" />
+          <SocialMediaIcon imgSrc={youtubeIcon} title="YouTube" /> 
+          </motion.div>
+        </div>
+
+        <div className="flex-1 flex items-center justify-center">
+          <motion.img
+            variants={scale()}
+            transition={transition()}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false }}
+            src={aboutIllustration}
+            alt=""
+            className="max-w-full sm:max-w-[401px]"
+          />
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full h-[1px] bg-divider" />
+    </div>
+  );
+};
+
+export default About;
