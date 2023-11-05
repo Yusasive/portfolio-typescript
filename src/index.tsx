5cbaee18844f851afdb9d53da36240ccdda08e29
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-11-05 14:54:36 +0100

    updatting the site pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import App from "./App";
 
@@ -7,3 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(<App />);
+
+//ServiceWorkerRegistration.registered();
+
+reportWebVitals();
